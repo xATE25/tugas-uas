36e771b5d0c5019db956c10a87f41666f8b15214
Ahmad Tegar Dwipa Yoga <92283118+xATE25@users.noreply.github.com>
2023-06-23 19:49:29 +0700

    Add files via upload

---
+++ TambahMenu.vue
@@ -0,0 +1,90 @@
+<template>
+    <div>
+        <navbar-component></navbar-component>
+        <sidebar-component></sidebar-component>
+        <div class="content-wrapper">
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0">Form Tambah Menu</h1>
+                        </div>          
+                    </div>
+                </div>
+            </div>
+
+            <div class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="card card-success">
+                                <div class="card-header">
+                                    <h3 class="card-title">Data Menu</h3>
+                                </div>
+                                <form @submit.prevent="tambah">
+                                    <div class="card-body">
+                                        <div class="form-group">
+                                            <label>Nama Menu</label>
+                                            <input type="text" class="form-control" v-model="Menu.nama">
+                                        </div>                                        
+                                        <div class="form-group">
+                                            <label>Harga</label>
+                                            <input type="text"  class="form-control" v-model="Harga.alamat">
+                                        </div>
+                                        <div class="form-group">
+                                            <div>
+                                                <label>Jenis</label>
+                                            </div>
+                                            <div class="btn-group btn-group-toggle" data-toggle="buttons">                                                
+                                                <label class="btn btn-secondary">
+                                                    <input type="radio" value="L" v-model="Menu.jenis"> Makanan
+                                                </label>
+                                                <label class="btn btn-secondary">
+                                                    <input type="radio" value="P" v-model="Menu.jenis"> Minuman
+                                                </label>
+                                            </div>
+                                        </div>
+                                       
+                                    </div>
+                                    <div class="card-footer">
+                                        <button type="submit" class="btn btn-primary">Simpan</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Vue from 'vue';
+
+Vue.use(axios);
+
+export default {
+    data() {
+        name:'IndexTambah',
+        return {
+            menu : [],
+        }
+    },
+     created(){
+        this.tambah_menu();
+     },
+     methods : {
+        tambah(){  
+            this.tambah_menu();
+        },
+        tambah_data(){    
+            axios.post('http://localhost:8000/api/createmenu', this.menu)
+                      .then(() =>
+
+                      )
+                    },
+                }
+    }
+</script>
